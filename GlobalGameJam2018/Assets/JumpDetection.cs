6dbf7cec843a74f4f48a536dd103a18f7a09126d
MoeJay <moejay5@yahoo.com>
2018-01-26 23:04:51 -0600

    movement Finished and Main Scene Created

---
+++ GlobalGameJam2018/Assets/JumpDetection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpDetection : MonoBehaviour
+{
+    [HideInInspector]
+    public bool isGrounded = false;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
