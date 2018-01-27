e1d5fd4acd275d6751201b8d28362f44c75c4ab8
Josh <jam-millsap@users.noreply.github.com>
2018-01-26 20:27:12 -0600

    Health Script
    add damage functions

--- GlobalGameJam2018/Assets/Scripts/playerHealth.cs
+++ GlobalGameJam2018/Assets/Scripts/playerHealth.cs
@@ -1,16 +1,24 @@
 ﻿using UnityEngine;
 
-public class playerHealth : MonoBehaviour
-{
+public class playerHealth : MonoBehaviour {
+
     public const int maxHealth = 100;
     public int currentHealth = maxHealth;
-    public void takeDamage(int amount)
-    {
-        currentHealth -= amount;
-        if (currentHealth <= 0)
-        {
+
+    public void takeDamage(int amount) {
+        if (currentHealth <= 5) {
             currentHealth = 0;
+            CancelInvoke();
             Debug.Log("Dead!");
         }
+        currentHealth -= amount;
+        Debug.Log("Took Damage");
+        Debug.Log("Player Health = " + currentHealth);
+    }
+    void Start() {
+        InvokeRepeating("damageOverTime", 0.0f, 1.0f);
+    }    
+    public void damageOverTime() {
+        takeDamage(1);
     }
 }
