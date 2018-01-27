313e14117ca9bf5df778296cd367b9786d18f85b
LukeDuhe <Luke7490@gmail.com>
2018-01-27 00:19:05 -0600

    Added back me scripts

---
+++ GlobalGameJam2018/Assets/Scripts/UpdateHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateHealth : MonoBehaviour {
+
+	public GameObject player;
+	public double currentHealth;
+	public double maxHealth;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		currentHealth = player.GetComponent<playerHealth> ().currentHealth;
+		maxHealth = player.GetComponent<playerHealth> ().maxHealth;
+		gameObject.GetComponent<Text> ().text = "Health: " + currentHealth.ToString() + " / " + maxHealth.ToString();
+	}
+}
---
+++ GlobalGameJam2018/Assets/Scripts/UpdateMedicine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateMedicine : MonoBehaviour {
+
+	public GameObject player;
+	public int medicine;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		medicine = player.GetComponent<playerHealth> ().medicine;
+		gameObject.GetComponent<Text> ().text = "Medicine: " + medicine.ToString();
+	}
+}
