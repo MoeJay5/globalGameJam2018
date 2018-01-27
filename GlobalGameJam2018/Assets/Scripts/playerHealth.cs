df0dac602656585a572c1c81fa0729a6a6f6518d
Josh <jam-millsap@users.noreply.github.com>
2018-01-26 23:25:02 -0600

    Fix Health

--- GlobalGameJam2018/Assets/Scripts/playerHealth.cs
+++ GlobalGameJam2018/Assets/Scripts/playerHealth.cs
@@ -2,15 +2,58 @@
 
 public class playerHealth : MonoBehaviour
 {
-    public const int maxHealth = 100;
-    public int currentHealth = maxHealth;
+
+    public double maxHealth = 100;
+    public double currentHealth;
+    public int food = 4;
+    public int water = 3;
+    public int medicine = 2;
+
     public void takeDamage(int amount)
     {
-        currentHealth -= amount;
         if (currentHealth <= 0)
         {
             currentHealth = 0;
+            CancelInvoke();
             Debug.Log("Dead!");
         }
+        currentHealth -= amount;
+        Debug.Log("Took Damage");
+        Debug.Log("Player Health = " + currentHealth);
+    }
+    void Start()
+    {
+        InvokeRepeating("damageOverTime", 0.0f, 1.0f);
+        currentHealth = maxHealth;
+    }
+    public void damageOverTime()
+    {
+        takeDamage(1);
+    }
+
+    public void rest()
+    {
+        double modifier = 0;
+        if (food >= 1)
+        {
+            modifier++;
+            food--;
+        }
+        if (water >= 1)
+        {
+            modifier++;
+            water--;
+        }
+        if (medicine >= 1)
+        {
+            modifier++;
+            medicine--;
+        }
+        currentHealth = maxHealth * (modifier / 3);
+        Debug.Log("Current Health" + currentHealth);
+        Debug.Log("Modifier " + modifier);
+        Debug.Log("Current Health: = " + currentHealth);
+        if (currentHealth <= 0)
+            Debug.Log("Dead!");
     }
 }
