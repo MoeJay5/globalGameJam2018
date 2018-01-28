bd04ee10f3adb296f16172cf6f345df6c24a6134
Unknown <vsurman123@gmail.com>
2018-01-28 00:02:33 -0600

    Added ambient noise and player movement sound

--- GlobalGameJam2018/Assets/_Scripts/LeaderManager.cs
+++ GlobalGameJam2018/Assets/_Scripts/LeaderManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class LeaderManager : MonoBehaviour
@@ -20,6 +21,11 @@ public class LeaderManager : MonoBehaviour
     private bool _jump;
 
     private Rigidbody rb;
+
+    public AudioSource[] playerSounds;
+    public AudioSource running;
+    public AudioSource ambient;
+    private bool isPlaying = false;
     
     void Start()
     {
@@ -34,6 +40,10 @@ public class LeaderManager : MonoBehaviour
             _speedMax = _speed;
 
         speedReset = _speed;
+
+        playerSounds = GetComponentsInChildren<AudioSource>();
+        running = playerSounds[0];
+        ambient = playerSounds[1];
     }
 
     void Update()
@@ -62,6 +72,8 @@ public class LeaderManager : MonoBehaviour
 
             _isMoving = true;
             _leader.position += _leader.forward * _speed * Time.deltaTime;
+
+            StartCoroutine(PlaySound(0));
         }
         else
         {
@@ -93,5 +105,17 @@ public class LeaderManager : MonoBehaviour
         _cameraAnimator.SetBool("jump", _jump);
         _cameraAnimator.SetFloat("movementSpeed", _speed);
         _cameraAnimator.SetBool("isMoving", _isMoving);
+    }
+
+    IEnumerator PlaySound(int clipNum)
+    {
+        if(!isPlaying)
+        {
+            isPlaying = true;
+            playerSounds[clipNum].Play();
+            yield return new WaitForSeconds(playerSounds[clipNum].clip.length);
+            isPlaying = false;
+            yield return null;
+        }
     }
 }
