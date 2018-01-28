06efa045c405d705a25a9d13c9ef5c690584a36f
MoeJay <moejay5@yahoo.com>
2018-01-28 13:52:19 -0600

    main Mech Impl

--- GlobalGameJam2018/Assets/Scripts/Pickups.cs
+++ GlobalGameJam2018/Assets/Scripts/Pickups.cs
@@ -6,53 +6,48 @@ public class Pickups : MonoBehaviour {
 
     public GameObject particle; 
 
-	// Use this for initialization
-	void Start () {
-        particle = GameObject.Find("PickupParticle");
-    }
-
     void OnTriggerEnter(Collider other) {
         if (other.tag.Equals("food"))
         {
             Destroy(other.gameObject);
-            Instantiate(particle);
+            Instantiate(particle, transform.position, transform.rotation);
             gameObject.GetComponent<playerHealth>().food++;
         }
         if (other.tag.Equals("medicine"))
         {
             Destroy(other.gameObject);
-            Instantiate(particle);
+            Instantiate(particle, transform.position, transform.rotation);
             gameObject.GetComponent<playerHealth>().medicine++;
         }
         if (other.tag.Equals("water"))
         {
             Destroy(other.gameObject);
-            Instantiate(particle);
+            Instantiate(particle, transform.position, transform.rotation);
             gameObject.GetComponent<playerHealth>().water++;
         }
 
           if (other.tag.Equals("controllerBox"))
         {
             Destroy(other.gameObject);
-            Instantiate(particle);
+            Instantiate(particle, transform.position, transform.rotation);
             gameObject.GetComponent<playerHealth>().controllerBox++;
         }
           if (other.tag.Equals("broadcastDish"))
         {
             Destroy(other.gameObject);
-            Instantiate(particle);
+            Instantiate(particle, transform.position, transform.rotation);
             gameObject.GetComponent<playerHealth>().broadcastDish++;
         }
           if (other.tag.Equals("battery"))
         {
             Destroy(other.gameObject);
-            Instantiate(particle);
+            Instantiate(particle, transform.position, transform.rotation);
             gameObject.GetComponent<playerHealth>().battery++;
         }
           if (other.tag.Equals("microphone"))
         {
             Destroy(other.gameObject);
-            Instantiate(particle);
+            Instantiate(particle, transform.position, transform.rotation);
             gameObject.GetComponent<playerHealth>().microphone++;
         }
     }
